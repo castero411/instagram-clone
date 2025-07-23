58e5e07d66db6c37e93dc78536b8530703bba9bd
castero411 <ahking491@gmail.com>
2025-07-23 16:54:02 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:instagram/themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: AppTheme.lightTheme,
-      home: AppTheme.lightTheme,
-    );
+    return MaterialApp();
   }
 }
